b31da905523479f29e3ccf0c6ec5a6729cb512ed
Shivam Mahajan <spmshivam@gmail.com>
2023-06-16 02:11:53 -0400

    Added the add, delete player functionality and also added scores functionality

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Home from "./Screens/Home";
-import Detail from "./Screens/detail";
+import Detail from "./Screens/Detail";
 
 const Stack = createStackNavigator();
 
@@ -10,7 +10,7 @@ export default function App(){
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
-      
+
         <Stack.Screen
           name="Home"
           component={Home}
